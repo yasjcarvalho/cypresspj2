ca6ff0503594df58936723ca6dbfe24cd491f205
Yasmin Jandriele <97072307+yasjcarvalho@users.noreply.github.com>
2023-08-15 15:17:15 -0300

    date

--- cypress/e2e/finances.cy.js
+++ cypress/e2e/finances.cy.js
@@ -3,9 +3,9 @@ describe('Transações', () => {
     cy.visit('https://devfinance-agilizei.netfy.app/#')
 
     cy.contains('Nova Transação').click()
-    cy.get('#description').type("Freela")
+    cy.get('#description').type('Freela')
     cy.get('amount').type(250)
-    cy.get("#date").type("2023-02-15") //yyyy-mm-dd
+    cy.get('#date').type('2023-02-15') //yyyy-mm-dd
 
     cy.contains('button', 'Salvar').click()
   })
