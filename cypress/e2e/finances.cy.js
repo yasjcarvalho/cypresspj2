e09e443f3c47d6196756ba6dfb6470c995acabb5
Yasmin Jandriele <97072307+yasjcarvalho@users.noreply.github.com>
2023-08-15 18:31:06 -0300

    asserts e hooks

--- cypress/e2e/finances.cy.js
+++ cypress/e2e/finances.cy.js
@@ -1,12 +1,41 @@
 describe('Transações', () => {
+  // hooks -> executar antes ou depois / de cada ou de todos os testes
+  // before
+  // after
+  // beforeEach
+  // afterEach
+
+  beforeEach(() => {
+    cy.visit('https://devfinance-agilizei.netlify.app/#')
+  })
+
   it('Cadastrar uma entrada', () => {
-    cy.visit('https://devfinance-agilizei.netfy.app/#')
+    criarTransacao('Freela', 250)
 
-    cy.contains('Nova Transação').click()
-    cy.get('#description').type('Freela')
-    cy.get('amount').type(250)
-    cy.get('#date').type('2023-02-15') //yyyy-mm-dd
+    cy.get('tbody tr td.description').should('have.text', 'Freela')
+  })
 
-    cy.contains('button', 'Salvar').click()
+  it('Cadastrar uma saída', () => {
+    criarTransacao('Cinema', -45)
+    cy.get('tbody tr td.description').should('have.text', 'Cinema')
+  })
+
+  it('Excluir transação', () => {
+    criarTransacao('Freela', 100)
+    cy.contains('.description', 'Freela')
+    .parent('') //volta  uma linha  para o elemento pai
+    .find('img')
+    .click()
   })
 })
+
+//criar função auxliar
+function criarTransacao(descricao, valor) {
+  cy.contains('Nova Transação').click()
+  cy.get('#description').type(descricao)
+  cy.get('#amount').type(valor)
+  cy.get('#date').type('2023-02-15') //yyyy-mm-dd
+  cy.contains('button', 'Salvar').click()
+}
+
+// https://docs.cypress.io/guides/references/assertions O QUE VALIDAR NO PROJETO?????
